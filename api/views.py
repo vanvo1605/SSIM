71dc863c1112a39caafa433a94bf0e2e76f4c338
20231599 <vavo001@ess.ais.ac.nz>
2024-07-08 14:26:23 +1200

    update

--- api/views.py
+++ api/views.py
@@ -16,7 +16,7 @@ def sumNumbers(start_num, end_num):
 @api_view(['Post'])
 def sumNumbersView(request):
     if request.method == 'POST':
-        start_num = request.data['start_num']
-        end_num = request.data['end_num']
+        start_num = int(request.data['start_num'])
+        end_num = int(request.data['end_num'])
         result = sumNumbers(start_num, end_num)
         return Response({'result': result})
